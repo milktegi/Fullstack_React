c51a8e65b0e6084c71f7b51c5798ab4fba52d23c
milktegi <milkteegi@gmail.com>
2018-11-30 16:49:08 +0900

    Google OAuth_ing

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/Keys');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hey: 'buddy!'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      cleintSercret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
